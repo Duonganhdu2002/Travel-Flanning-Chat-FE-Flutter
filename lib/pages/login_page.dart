3e1d15f650458789bd7585ef9afe44995eb1de4c
Duonganhdu2002 <Wydanhdu@gmail.com>
2024-04-20 23:46:41 +0700

    Add Login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/pages/on_bording_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -10,8 +12,47 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("This is Login form"),
+    return Scaffold(
+      appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 12.0),
+          child: BackButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const OnBordingPage(),
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text(
+              "Sign in now",
+              style: GoogleFonts.roboto(
+                fontSize: 35,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Please sign in to continue our app",
+              style: GoogleFonts.roboto(
+                fontSize: 16,
+                color: Colors.black45,
+              ),
+            ),
+
+            // Orther coponents here
+          ],
+        ),
+      ),
     );
   }
 }
