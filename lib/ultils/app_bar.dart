2b400208ad954b510692fc71dd9e10a21c35a3c8
Duonganhdu2002 <Wydanhdu@gmail.com>
2024-04-30 12:38:11 +0700

    Adding code

--- lib/ultils/app_bar.dart
+++ lib/ultils/app_bar.dart
@@ -4,19 +4,23 @@ class CustomBar extends StatelessWidget implements PreferredSizeWidget {
   final Widget? leftWidget;
   final Widget? centerWidget;
   final Widget? rightWidget;
+  final Color? backgroundColor;
+  final bool isTransparent;
 
   const CustomBar({
     super.key,
     this.leftWidget,
     this.centerWidget,
     this.rightWidget,
+    this.backgroundColor = const Color(0xFFF5F5F5),
+    this.isTransparent = false,
   });
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
       automaticallyImplyLeading: false,
-      backgroundColor: Colors.grey[100],
+      backgroundColor: isTransparent ? Colors.transparent : backgroundColor,
       leadingWidth: double.infinity,
       leading: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 12.0),
