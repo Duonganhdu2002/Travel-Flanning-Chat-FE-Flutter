c63ffab3f568335467b8e19a91a3cf521460c3ce
con Tom <lequoctamle55@gmail.com>
2024-05-02 16:15:16 +0700

    ....

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/components/app_bar.dart';
 import 'package:flutter_app/components/back_icon.dart';
