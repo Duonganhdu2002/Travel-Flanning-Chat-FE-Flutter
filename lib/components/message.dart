88e3b6ca5213ad23f60fe545f3b2a171d89f2d70
con Tom <lequoctamle55@gmail.com>
2024-05-02 16:15:07 +0700

    ....

--- lib/components/message.dart
+++ lib/components/message.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/pages/chat_page.dart';
-import 'package:flutter_app/pages/home_page.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class MessageComponent extends StatelessWidget {
--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/components/app_bar.dart';
 import 'package:flutter_app/components/back_icon.dart';
+import 'package:flutter_app/components/call_icon.dart';
 
 class ChatPage extends StatefulWidget {
   const ChatPage({super.key});
@@ -15,11 +17,18 @@ class _ChatPageState extends State<ChatPage> {
     return const Scaffold(
       appBar: CustomBar(
         leftWidget: BackIcon(),
-        centerWidget1: Text("Aloha"),
+        centerWidget1: Text("Aloha",
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+        centerWidget2: Text("Active now",
+            style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.green)),
+        rightWidget: CallIcon(),
       ),
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 10),
-        ),
+      ),
     );
   }
 }
