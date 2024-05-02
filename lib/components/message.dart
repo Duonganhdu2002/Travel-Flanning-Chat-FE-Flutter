c7b6afcdcc1e1e9f07f350285d05728a2705e95c
con Tom <lequoctamle55@gmail.com>
2024-05-02 14:39:08 +0700

    ....

--- lib/components/message.dart
+++ lib/components/message.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/pages/home_page.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class MessageComponent extends StatelessWidget {
@@ -79,32 +80,8 @@ class MessageComponent extends StatelessWidget {
             Expanded(
               child: ListView(
                 children: [
-                  itemMessage("lib/images/User_img.png", "HAHAHAHA", "aaaaaaaa",
-                      1, "07:76"),
-                  itemMessage("lib/images/User_img.png", "HAHAHAHA", "aaaaaaaa",
-                      1, "07:76"),
-                  itemMessage("lib/images/User_img.png", "HAHAHAHA", "aaaaaaaa",
-                      1, "07:76"),
-                  itemMessage("lib/images/User_img.png", "HAHAHAHA", "aaaaaaaa",
-                      1, "07:76"),
-                  itemMessage("lib/images/User_img.png", "HAHAHAHA", "aaaaaaaa",
-                      1, "07:76"),
-                  itemMessage("lib/images/User_img.png", "HAHAHAHA", "aaaaaaaa",
-                      1, "07:76"),
-                  itemMessage("lib/images/User_img.png", "HAHAHAHA", "aaaaaaaa",
-                      1, "07:76"),
-                  itemMessage("lib/images/User_img.png", "HAHAHAHA", "aaaaaaaa",
-                      1, "07:76"),
-                  itemMessage("lib/images/User_img.png", "HAHAHAHA", "aaaaaaaa",
-                      1, "07:76"),
-                  itemMessage("lib/images/User_img.png", "HAHAHAHA", "aaaaaaaa",
-                      1, "07:76"),
-                  itemMessage("lib/images/User_img.png", "HAHAHAHA", "aaaaaaaa",
-                      1, "07:76"),
-                  itemMessage("lib/images/User_img.png", "HAHAHAHA", "aaaaaaaa",
-                      1, "07:76"),
-                  itemMessage("lib/images/User_img.png", "HAHAHAHA", "aaaaaaaa",
-                      1, "07:76"),
+                  itemMessage(context, "lib/images/User_img.png", "HAHAHAHA",
+                      "aaaaaaaa", 1, "07:76"),
                 ],
               ),
             )
@@ -115,6 +92,7 @@ class MessageComponent extends StatelessWidget {
   }
 
   Widget itemMessage(
+    BuildContext context,
     String pathImage,
     String nameUser,
     String showMessage,
@@ -123,75 +101,83 @@ class MessageComponent extends StatelessWidget {
   ) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 25.0),
-      child: Row(
-        children: [
-          Expanded(
-            child: Row(
-              children: [
-                SizedBox(
-                  width: 60,
-                  child: Image.asset(
-                    pathImage,
+      child: InkWell(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const HomePage()),
+          );
+        },
+        child: Row(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  SizedBox(
                     width: 60,
-                    height: 60,
-                    fit: BoxFit.cover,
+                    child: Image.asset(
+                      pathImage,
+                      width: 60,
+                      height: 60,
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                ),
-                const SizedBox(
-                  width: 20,
-                ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            nameUser,
-                            style: const TextStyle(
-                                color: Color(0xFF1B1E28),
-                                fontSize: 20,
-                                fontWeight: FontWeight.w500),
-                          ),
-                          ImageFiltered(
-                            imageFilter: const ColorFilter.mode(
-                              Color(0xFF7D848D),
-                              BlendMode.srcATop,
-                            ),
-                            child: SvgPicture.asset(
-                              statusMessage == 1
-                                  ? "lib/images/sent.svg"
-                                  : statusMessage == 2
-                                      ? "lib/images/received.svg"
-                                      : "lib/images/seen.svg",
-                              width: 14,
-                              height: 14,
-                            ),
-                          ),
-                        ],
-                      ),
-                      Text(
-                        showMessage,
-                        style: const TextStyle(color: Color(0xFF7D848D)),
-                      ),
-                    ],
+                  const SizedBox(
+                    width: 20,
                   ),
-                )
-              ],
-            ),
-          ),
-          SizedBox(
-            width: 60,
-            child: Text(
-              timeSend,
-              textAlign: TextAlign.end,
-              style: const TextStyle(
-                color: Color(0xFF7D848D),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              nameUser,
+                              style: const TextStyle(
+                                  color: Color(0xFF1B1E28),
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                            ImageFiltered(
+                              imageFilter: const ColorFilter.mode(
+                                Color(0xFF7D848D),
+                                BlendMode.srcATop,
+                              ),
+                              child: SvgPicture.asset(
+                                statusMessage == 1
+                                    ? "lib/images/sent.svg"
+                                    : statusMessage == 2
+                                        ? "lib/images/received.svg"
+                                        : "lib/images/seen.svg",
+                                width: 14,
+                                height: 14,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Text(
+                          showMessage,
+                          style: const TextStyle(color: Color(0xFF7D848D)),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
               ),
             ),
-          ),
-        ],
+            SizedBox(
+              width: 60,
+              child: Text(
+                timeSend,
+                textAlign: TextAlign.end,
+                style: const TextStyle(
+                  color: Color(0xFF7D848D),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
